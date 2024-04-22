b0195768a8a9ddbc61854862c5a4de1d45555a42
HiImDavidH <156952021+HiImDavidH@users.noreply.github.com>
2024-04-22 02:42:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,24 +8,43 @@ app.use(express.json())
 app.use(express.static('public'))
 
 const todos = [
-	{ id: 1, item: 'Learn JavaScript', complete: false },
-	{ id: 2, item: 'Learn Express', complete: false },
-	{ id: 3, item: 'Build a To Do App', complete: false }
+    { id: 1, item: 'Learn JavaScript', complete: false },
+    { id: 2, item: 'Learn Express', complete: false },
+    { id: 3, item: 'Build a To Do App', complete: false }
 ]
 
 app.get('/', (_, response) => {
-	response.sendFile('index.html', { root })
+    response.sendFile('index.html', { root })
 })
-
 
 
 // GET /api/todos
 
+app.get('/api/todos', (request, response) => {
+    // request = fetch('/')
+    
+    response.json(todos) 
+
+})
+
 // POST /api/todos
+
+app.post('/api/todos',  (request, response) => {
+    const { item } = request.body
+    todos.push({id: todos.length, item:item, complete: false})
+    response.json({ id:todos.length })
+})
 
 // PUT /api/todos/:id
 
+app.put('/api/todos/:id', (request, response) => {
+const { id } = request.params
+const task = todos.find(todo => todo.id.toString() === id)
+task.complete = !task.complete // toggle the complete property
+response.json({ id:task.id, complete:task.complete})
 
+})
 
 const message = `Server running: http://localhost:${port}`
 app.listen(port, () => console.log(message))
+
